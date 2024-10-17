33a37a565f5791b2ba02706873ddff1fbbdf3313
鸳汐 <39462133+DreamedWorker@users.noreply.github.com>
2024-10-17 09:45:39 +0800

    修复了账号信息板块在滑动时严重卡顿掉帧（CPU占用率100%）的问题

---
+++ TravellersBag/Core/Config/DeviceInfoPane.swift
@@ -0,0 +1,18 @@
+//
+//  DeviceInfoPane.swift
+//  TravellersBag
+//
+//  Created by 鸳汐 on 2024/10/15.
+//
+
+import SwiftUI
+
+struct DeviceInfoPane: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DeviceInfoPane()
+}
--- TravellersBag/Screens/Account/AccountDetail.swift
+++ TravellersBag/Screens/Account/AccountDetail.swift
@@ -32,77 +32,81 @@ struct AccountDetail: View {
     var body: some View {
         if showEverything {
             ScrollView {
-                KFImage(URL(string: account.shequHead!))
-                    .placeholder({ Image(systemName: "wifi").symbolRenderingMode(.multicolor) })
-                    .loadDiskFileSynchronously(true)
-                    .resizable()
-                    .frame(width: 96, height: 96)
-                    .aspectRatio(contentMode: .fill)
-                    .clipShape(Circle())
-                    .padding(.top, 16)
-                Text(account.shequNicname!).font(.title2).bold().padding(.bottom, 16)
-                Form {
-                    HStack(spacing: 8, content: {
-                        Image(systemName: "person.text.rectangle")
-                        Text("account.detail.genshin_uid")
+                VStack {
+                    KFImage(URL(string: account.shequHead!))
+                        .placeholder({ Image(systemName: "wifi").symbolRenderingMode(.multicolor) })
+                        .loadDiskFileSynchronously(true)
+                        .resizable()
+                        .frame(width: 96, height: 96)
+                        .aspectRatio(contentMode: .fill)
+                        .clipShape(Circle())
+                        .padding(.top, 16)
+                    Text(account.shequNicname!).font(.title2).bold().padding(.bottom, 16)
+                    GroupBox("app.name") {
+                        HStack(spacing: 8, content: {
+                            Image(systemName: "person.text.rectangle")
+                            Text("account.detail.genshin_uid")
+                            Spacer()
+                            Text(account.genshinUID!).foregroundStyle(.secondary)
+                        }).padding(4)
+                        HStack(spacing: 8, content: {
+                            Image(systemName: "tag")
+                            Text("account.detail.genshin_name")
+                            Spacer()
+                            Text(account.genshinNicname!).foregroundStyle(.secondary)
+                        }).padding(4)
+                        HStack(spacing: 8, content: {
+                            Image(systemName: "person.circle")
+                            Text("account.detail.genshin_head")
+                            Spacer()
+                            if String(imgAddress.split(separator: "@")[0]) == "C"  {
+                                KFImage(URL(string: String(imgAddress.split(separator: "@")[1])))
+                                    .placeholder({ Image(systemName: "wifi").symbolRenderingMode(.multicolor) })
+                                    .loadDiskFileSynchronously(true)
+                                    .resizable()
+                                    .frame(width: 24, height: 24)
+                                    .aspectRatio(contentMode: .fill)
+                                    .clipShape(Circle())
+                            } else {
+                                Image(nsImage: NSImage(contentsOfFile: String(imgAddress.split(separator: "@")[1]))!)
+                                    .resizable().scaledToFill().frame(width: 24, height: 24).aspectRatio(contentMode: .fill).clipShape(Circle())
+                            }
+                        }).padding(4)
+                        HStack(spacing: 8, content: {
+                            Image(systemName: "level")
+                            Text("account.detail.genshin_level")
+                            Spacer()
+                            Text(account.level!).foregroundStyle(.secondary)
+                        }).padding(4)
+                    }.padding(.bottom, 8)
+                    GroupBox("app.confirm") {
+                        HStack(spacing: 8, content: {
+                            Image(systemName: "person.text.rectangle")
+                            Text("account.detail.shequ_uid")
+                            Spacer()
+                            Text(account.stuid!).foregroundStyle(.secondary)
+                        }).padding(4)
+                        HStack(spacing: 8, content: {
+                            Image(systemName: "ellipsis.rectangle")
+                            Text("account.detail.shequ_mid")
+                            Spacer()
+                            Text(account.mid!).foregroundStyle(.secondary)
+                        }).padding(4)
+                    }.padding(.bottom, 8)
+                    HStack {
+                        Button("account.detail.logout", action: { showLogout = true })
                         Spacer()
-                        Text(account.genshinUID!).foregroundStyle(.secondary)
-                    })
-                    HStack(spacing: 8, content: {
-                        Image(systemName: "tag")
-                        Text("account.detail.genshin_name")
-                        Spacer()
-                        Text(account.genshinNicname!).foregroundStyle(.secondary)
-                    })
-                    HStack(spacing: 8, content: {
-                        Image(systemName: "person.circle")
-                        Text("account.detail.genshin_head")
-                        Spacer()
-                        if String(imgAddress.split(separator: "@")[0]) == "C"  {
-                            KFImage(URL(string: String(imgAddress.split(separator: "@")[1])))
-                                .placeholder({ Image(systemName: "wifi").symbolRenderingMode(.multicolor) })
-                                .loadDiskFileSynchronously(true)
-                                .resizable()
-                                .frame(width: 24, height: 24)
-                                .aspectRatio(contentMode: .fill)
-                                .clipShape(Circle())
-                        } else {
-                            Image(nsImage: NSImage(contentsOfFile: String(imgAddress.split(separator: "@")[1]))!)
-                                .resizable().scaledToFill().frame(width: 24, height: 24).aspectRatio(contentMode: .fill).clipShape(Circle())
-                        }
-                    })
-                    HStack(spacing: 8, content: {
-                        Image(systemName: "level")
-                        Text("account.detail.genshin_level")
-                        Spacer()
-                        Text(account.level!).foregroundStyle(.secondary)
-                    })
-                }.formStyle(.grouped).scrollDisabled(true)
-                Form {
-                    HStack(spacing: 8, content: {
-                        Image(systemName: "person.text.rectangle")
-                        Text("account.detail.shequ_uid")
-                        Spacer()
-                        Text(account.stuid!).foregroundStyle(.secondary)
-                    })
-                    HStack(spacing: 8, content: {
-                        Image(systemName: "ellipsis.rectangle")
-                        Text("account.detail.shequ_mid")
-                        Spacer()
-                        Text(account.mid!).foregroundStyle(.secondary)
-                    })
-                }.formStyle(.grouped).scrollDisabled(true)
-                HStack {
-                    Button("account.detail.logout", action: { showLogout = true })
-                    Button("account.detail.copy_cookie", action: {
-                        let context = "stuid=\(account.stuid!);stoken=\(account.stoken!);mid=\(account.mid!)"
-                        let board = NSPasteboard.general
-                        board.clearContents()
-                        board.setData(context.data(using: .utf8), forType: .string)
-                        GlobalUIModel.exported.makeAnAlert(type: 1, msg: "操作完成")
-                    })
-                    Button("account.detail.def_account", action: setDefaultAccount)
-                }.padding(.horizontal).padding(.bottom, 16)
+                        Button("account.detail.copy_cookie", action: {
+                            let context = "stuid=\(account.stuid!);stoken=\(account.stoken!);mid=\(account.mid!)"
+                            let board = NSPasteboard.general
+                            board.clearContents()
+                            board.setData(context.data(using: .utf8), forType: .string)
+                            GlobalUIModel.exported.makeAnAlert(type: 1, msg: "操作完成")
+                        })
+                        Button("account.detail.def_account", action: setDefaultAccount)
+                    }.padding(.horizontal).padding(.bottom, 16)
+                }
+                .padding(.horizontal, 16)
             }
             .alert(
                 "app.warning", isPresented: $showLogout,
