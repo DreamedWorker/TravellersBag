649f3457c9f33e9b2680f3941bb24d823809349c
鸳汐 <39462133+DreamedWorker@users.noreply.github.com>
2024-10-24 13:38:45 +0800

    Initial Commit

---
+++ TravellersBag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravellersBag
+//
+//  Created by 鸳汐 on 2024/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravellersBag/TravellersBagApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravellersBagApp.swift
+//  TravellersBag
+//
+//  Created by 鸳汐 on 2024/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TravellersBagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
